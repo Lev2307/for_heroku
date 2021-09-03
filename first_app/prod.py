d131db9257384cefde86d1623365df9dc7474f39
Lev2307 <levchik23072007@gmail.com>
2021-09-03 17:25:53 +0300

    update

--- first_app/prod.py
+++ first_app/prod.py
@@ -4,6 +4,7 @@ ALLOWED_HOSTS = ["https://git.heroku.com/young-bayou-30157.git"]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
